b98cf71b555d3f7458797e7ce541e129dd154b5c
André França <andrewf992@gmail.com>
2022-12-27 17:37:20 -0300

    Trying to make the ReactSlider work. Failed.

--- src/Components/Settings.tsx
+++ src/Components/Settings.tsx
@@ -5,6 +5,12 @@ function Settings() {
   return(
     <div style={{textAlign:'left'}}>
       <label> Work minutes: </label>
+      <ReactSlider
+        className="horizontal-slider"
+        thumbClassName="example-thumb"
+        trackClassName="example-track"
+        renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+    />
       
 
       <label> Break minutes: </label>
